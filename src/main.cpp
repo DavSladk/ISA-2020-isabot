27cc2c0a58e8ea658f0fb7d867bb8b5c41456fc6
Mangovnik <davsladk@gmail.com>
2020-11-05 22:05:29 +0100

    Main.cpp formatting fix.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <iostream>
+#include "Options.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
     cout << "I AM THE MIGHTY ISABOT! BOW BEFORE ME!" << endl;
     return 0;
