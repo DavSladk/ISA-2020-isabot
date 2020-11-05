05e97450dd254241187fe8bffe85540f1546c0a9
Mangovnik <davsladk@gmail.com>
2020-11-05 22:05:56 +0100

    Options class skeleton added.

---
+++ headers/Options.h
@@ -0,0 +1,18 @@
+#ifndef XSLADK07_OPTIONS_H
+#define XSLADK07_OPTIONS_H
+
+#include <unistd.h>
+#include <string>
+#include <iostream>
+
+#include "CustomException.h"
+
+class Options
+{
+    public:
+
+    private:
+
+};
+
+#endif //XSLADK07_OPTIONS_H
