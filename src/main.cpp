8b7a2a650c5f67325da1bf1cc8dd5557acb4e9c4
Mangovnik <davsladk@gmail.com>
2020-11-06 00:34:33 +0100

    main now runs the program

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "Options.h"
+#include "isabot.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    cout << "I AM THE MIGHTY ISABOT! BOW BEFORE ME!" << endl;
+    try
+    {
+        ISAbot bot(argc, argv);
+    }
+    catch (CustomException e)
+    {
+        std::cout << e.Message() << std::endl;
+    }
     return 0;
 }
