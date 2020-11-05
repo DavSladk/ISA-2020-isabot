7c57be5d8d9f67c6b94e8a2ee798d6bd6587992c
Mangovnik <davsladk@gmail.com>
2020-11-05 23:30:50 +0100

    Implemented options class

--- headers/Options.h
+++ headers/Options.h
@@ -1,18 +1,35 @@
 #ifndef XSLADK07_OPTIONS_H
 #define XSLADK07_OPTIONS_H
 
-#include <unistd.h>
 #include <string>
 #include <iostream>
+
+#include <cstring>
 
 #include "CustomException.h"
 
 class Options
 {
     public:
+        Options(int argc, char** argv);
+
+        bool helpIsSet();
+        bool verboseIsSet();
+        bool tokenIsSet();
+        std::string getToken();
 
     private:
+        bool helpFlag = false;
+        bool verboseFlag = false;
+        bool tokenFlag = false;
+        std::string token = "";
 
+        int argc;
+        char** argv;
+
+        void parseOptions();
+
+        const int MATCH = 0;
 };
 
 #endif //XSLADK07_OPTIONS_H
--- src/Options.cpp
+++ src/Options.cpp
@@ -1,2 +1,63 @@
 #include "Options.h"
 
+Options::Options(int argc, char** argv)
+{
+    this->argc = argc;
+    this->argv = argv;
+}
+
+bool Options::helpIsSet()
+{
+    return this->helpFlag;
+}
+
+bool Options::verboseIsSet()
+{
+    return this->verboseFlag;
+}
+
+bool Options::tokenIsSet()
+{
+    return this->tokenFlag;
+}
+
+std::string Options::getToken()
+{
+    return this->token;
+}
+
+void Options::parseOptions()
+{
+    for (int i = 1; i < this->argc; i++)
+    {
+        if ( strcmp(argv[i], "-h") == MATCH || strcmp(argv[i], "--help") == MATCH)
+        {
+            helpFlag = true;
+        }
+        else if ( strcmp(argv[i], "-v") == MATCH || strcmp(argv[i], "--verbose") == MATCH)
+        {
+            verboseFlag = true;
+        }
+        else if ( strcmp(argv[i], "-t") == MATCH)
+        {
+            if ( argc == i )
+            {
+                throw new CustomException("Missing token after -t option.", MISSING_TOKEN);
+            }
+            tokenFlag = true;
+            token = argv[++i];
+            i++;
+        }
+        else
+        {
+            std::string msg = "Unknow parametr" + std::string(argv[i]) + "on position" + std::to_string(i);
+            throw new CustomException(msg, OPTION_UNKNOW);
+        }
+        
+    }
+
+    if ( token == "")
+    {
+        throw new CustomException("Token can not be empty.", MISSING_TOKEN);
+    }
+}
